1b1fcb18a58ce0c7c901f794a48ac636e346b0c6
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2020-04-08 19:54:56 -0700

    first commit, added intention to filter by country name

---
+++ lambda/lambda_function.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+"""Simple fact sample app."""
+
+import logging
+import json
+import prompts
+import requests
+
+from datetime import date
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import (
+    AbstractRequestHandler, AbstractExceptionHandler,
+    AbstractRequestInterceptor, AbstractResponseInterceptor)
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model import Response
+
+sb = SkillBuilder()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+# Built-in Intent Handlers
+class get_stats_for_country(AbstractRequestHandler):
+    """Handler for Skill Launch and get_stats Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_request_type("LaunchRequest")(handler_input) or
+                is_intent_name("getStatsForCountry_Intent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In getStats_Intent")
+        
+        url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
+        
+        slots = handler_input.request_envelope.request.intent.slots
+        country = slots['country'].value
+        us_list = ['United States', 'united states', 'United States of America', 'united states of america', 'usa', 'USA', 'states']
+        
+        if country in us_list:
+            country = 'us'
+        
+        querystring = {"country":country}
+        headers = {
+            'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com",
+            'x-rapidapi-key': "771b5c2bdfmsh2e2155eb3d8bfdbp17e815jsn187644a490ab"
+            }
+            
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        data = response.json()
+        
+        error_check = data['error']
+        status_check = data['statusCode']
+        found_country_check = data['message']
+        
+        rules = [error_check == False,
+                status_check == 200]
+        
+        if all(rules):
+            if found_country_check == "Country not found. Returning all stats. Please use a country name found in the data property.":
+                speech = "Sorry, but I couldn't find " + country + " among countries, can you please repeat?"
+            else:
+                data_country = response.json()['data']['covid19Stats']
+                confirmed = 0 
+                deaths = 0 
+                recovered = 0 
+                
+                for province in data_country:
+                    confirmed += province['confirmed']
+                    deaths += province['deaths']
+                    recovered += province['recovered']
+                
+                deaths_per = round(deaths/confirmed * 100)
+                recovered_per = round(recovered/confirmed * 100)
+                
+                today = date.today().strftime("%B %d, %Y")
+                if country == 'us':
+                    country = 'united states'
+                speech = 'For ' + country + ', confirmed cases ' + str(confirmed) + ', died ' + str(deaths_per) + '% and recovered ' + str(recovered_per) + '% as of today, ' + str(today)
+            
+        else:
+            speech = data[prompts.EXCEPTION_MESSAGE]
+                
+        
+        handler_input.response_builder.speak(speech)
+        return handler_input.response_builder.response
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In HelpIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.HELP_MESSAGE]
+        reprompt = data[prompts.HELP_REPROMPT]
+        handler_input.response_builder.speak(speech).ask(
+            reprompt)
+        return handler_input.response_builder.response
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In CancelOrStopIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.STOP_MESSAGE]
+        handler_input.response_builder.speak(speech)
+        return handler_input.response_builder.response
+
+
+class FallbackIntentHandler(AbstractRequestHandler):
+    """Handler for Fallback Intent.
+
+    AMAZON.FallbackIntent is only available in en-US locale.
+    This handler will not be triggered except in that locale,
+    so it is safe to deploy on any locale.
+    """
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In FallbackIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.FALLBACK_MESSAGE]
+        reprompt = data[prompts.FALLBACK_REPROMPT]
+        handler_input.response_builder.speak(speech).ask(
+            reprompt)
+        return handler_input.response_builder.response
+
+
+class LocalizationInterceptor(AbstractRequestInterceptor):
+    """
+    Add function to request attributes, that can load locale specific data.
+    """
+
+    def process(self, handler_input):
+        locale = handler_input.request_envelope.request.locale
+        logger.info("Locale is {}".format(locale))
+
+        # localized strings stored in language_strings.json
+        with open("language_strings.json") as language_prompts:
+            language_data = json.load(language_prompts)
+        # set default translation data to broader translation
+        if locale[:2] in language_data:
+            data = language_data[locale[:2]]
+            # if a more specialized translation exists, then select it instead
+            # example: "fr-CA" will pick "fr" translations first, but if "fr-CA" translation exists,
+            # then pick that instead
+            if locale in language_data:
+                data.update(language_data[locale])
+        else:
+            data = language_data[locale]
+        handler_input.attributes_manager.request_attributes["_"] = data
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In SessionEndedRequestHandler")
+
+        logger.info("Session ended reason: {}".format(
+            handler_input.request_envelope.request.reason))
+        return handler_input.response_builder.response
+
+
+# Exception Handler
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Catch all exception handler, log exception and
+    respond with custom message.
+    """
+
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.info("In CatchAllExceptionHandler")
+        logger.error(exception, exc_info=True)
+
+        handler_input.response_builder.speak(EXCEPTION_MESSAGE).ask(
+            HELP_REPROMPT)
+
+        return handler_input.response_builder.response
+
+
+# Request and Response loggers
+class RequestLogger(AbstractRequestInterceptor):
+    """Log the alexa requests."""
+
+    def process(self, handler_input):
+        # type: (HandlerInput) -> None
+        logger.debug("Alexa Request: {}".format(
+            handler_input.request_envelope.request))
+
+
+class ResponseLogger(AbstractResponseInterceptor):
+    """Log the alexa responses."""
+
+    def process(self, handler_input, response):
+        # type: (HandlerInput, Response) -> None
+        logger.debug("Alexa Response: {}".format(response))
+
+
+# Register intent handlers
+sb.add_request_handler(get_stats_for_country())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+
+# Register exception handlers
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+# Register request and response interceptors
+sb.add_global_request_interceptor(LocalizationInterceptor())
+sb.add_global_request_interceptor(RequestLogger())
+sb.add_global_response_interceptor(ResponseLogger())
+
+# Handler name that is used on AWS lambda
+lambda_handler = sb.lambda_handler()
